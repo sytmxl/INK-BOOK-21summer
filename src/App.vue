18b06275d31a8c57e14784752bc3bd20841309ee
Ma <1263122871@qq.com>
2022-08-01 16:27:00 +0800

    route

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,33 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+ <div id="app">
+   <router-view v-if="isRouterAlive"/>
+ </div>
+ 
 </template>
+ 
+<script>
+export default {
+ name: 'App',
+ provide () {
+   return{
+     reload: this.reload
+   }
+ },
+ data() {
+   return {
+       isRouterAlive: true
+   }
+ },
+ methods:{
+   reload(){
+     this.isRouterAlive = false
+     this.$nextTick(function(){
+       this.isRouterAlive = true
+     })
+   }
+ }
+}
+</script>
 
 <style>
 #app {
@@ -14,19 +35,11 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: gray;
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+* {
+  margin: 0;
+  padding: 0;
 }
 </style>
+
---
+++ src/views/welcome.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="init">
+    这是欢迎页面
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+#init{
+    margin:20px auto;
+    color:blueviolet;
+    font-size: large;
+    font-weight: bold;
+}
+</style>
