793347f8af2aae2c7bc347bcec961976a16d039c
Ma <1263122871@qq.com>
2022-08-02 18:05:38 +0800

    huanying

--- src/main.js
+++ src/main.js
@@ -6,6 +6,15 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 
+// 全局引入wow,animate
+import "animate.css"
+import "wowjs/css/libs/animate.css"
+import wow from 'wowjs'
+// 作为一个$参数
+Vue.prototype.$wow = wow
+    // 引入jquery
+
+
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 new Vue({
