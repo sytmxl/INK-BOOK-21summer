440b7136d3d7fbca225183ce95a1108094323259
HuBoXuan <1450858017@qq.com>
2022-08-09 15:30:21 +0800

    beautify scoller

--- src/App.vue
+++ src/App.vue
@@ -98,5 +98,32 @@ el-icon:hover, el-icon-close:hover {
   color: #35424e !important;
   background: rgba(191, 202, 217, 0.495) !important;
 }
+::-webkit-scrollbar {
+  width: 10px;
+  height: 10px;
+}
+
+::-webkit-scrollbar-track {
+  width: 6px;
+  background: rgba(#101F1C, 0.1);
+  -webkit-border-radius: 2em;
+  -moz-border-radius: 2em;
+  border-radius: 2em;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: rgba(144,147,153,.5);
+  background-clip: padding-box;
+  min-height: 28px;
+  -webkit-border-radius: 2em;
+  -moz-border-radius: 2em;
+  border-radius: 2em;
+  transition: background-color .3s;
+  cursor: pointer;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background-color: rgba(144,147,153,.3);
+}
 </style>
 
